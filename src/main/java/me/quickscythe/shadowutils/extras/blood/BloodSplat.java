ede6404229ba297abe63f0060491b098b0d8ff09
QuickScythe <QuickScythe@gmail.com>
2024-08-10 21:30:41 -0500

    test

--- src/main/java/me/quickscythe/shadowutils/extras/blood/BloodSplat.java
+++ src/main/java/me/quickscythe/shadowutils/extras/blood/BloodSplat.java
@@ -26,9 +26,14 @@ public class BloodSplat {
         int radius = (int) Math.floor(damage_taken/7);
         Utils.getLogger().log("Radius: " + radius);
 
-        for(int i=0;i<=damage_taken*r.nextInt(1)+1;i++){
-
-            Location splat_loc = loc.clone().add((radius > 0 ? r.nextInt(radius) : 0)+r.nextDouble(),0.01,(radius > 0 ? r.nextInt(radius) : 0)+r.nextDouble());
+        int loops = (int) (damage_taken*r.nextInt(1)+1);
+        Utils.getLogger().log("Loops: " + loops);
+        for(int i=0;i<=loops;i++){
+            double x = (r.nextBoolean() ? 1 : -1)*(radius > 0 ? r.nextInt(radius) : 0)+r.nextDouble();
+            double z = (r.nextBoolean() ? 1 : -1)*(radius > 0 ? r.nextInt(radius) : 0)+r.nextDouble();
+            Utils.getLogger().log("X: " + x);
+            Utils.getLogger().log("Z: " + z);
+            Location splat_loc = loc.clone().add(x,0.01,z);
             splat_loc.setPitch(-90);
             TextDisplay splat = loc.getWorld().spawn(splat_loc, TextDisplay.class);
             splat.text(Component.text("B"));
